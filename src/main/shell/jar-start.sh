79d3112732b0810a211f8e87d41908ba5eff3f42
zhanglianyu <zhanglylx@163.com>
2022-09-04 11:08:51 +0800

    更加jar包启动脚本

--- src/main/shell/jar-start.sh
+++ src/main/shell/jar-start.sh
@@ -1,45 +1,70 @@
 #!/bin/bash
 #需要关闭和启动的java包
-app=ruoyi-auth
+app=''
 #项目的上级目录
-path=/data/apps/java
-#java包存放目录
-javaPath=$path/$app
-#控制台日志存放目录
-consolePath=$javaPath/logs
-#启动时间
-startTime=$(date +%Y%m%d)
-#jar指定环境
-active=prod
-echo this is app : $app
-#判断日志目录是否存在，不存在创建
-if [ ! -f $javaPath/$app.jar ];
+path=`pwd`
+while getopts ":j:p:" opt
+ do
+  case $opt in
+      j)
+      app="$OPTARG";;
+      p)
+      path="$OPTARG";;
+      *)
+      echo "$OPTARG 无效参数";;
+  esac
+done
+if [[ ! $path =~ /$ ]]; then path=$path'/' ;fi
+if [ "$app" = "" ]; then
+     echo app is null,The current directory is searched by default
+     app=`find $path -maxdepth 1  -name "*.jar"`
+     if [ "$app" = "" ]; then
+       echo 没有找到当前目录下的jar: $path
+       exit 1
+     fi
+     #去除前缀目录
+     app=${app: ${#path}}
+fi
+if [[ ! $app =~ '.jar'$ ]]; then app=$app'.jar' ;fi
+
+#java包
+java=$path$app
+echo 'jar包:' $java
+if [ ! -f $java ];
 then
-    echo this is $javaPath/$app.jar not found
+    echo this is $java not found
     exit 1
 fi
+#启动时间
+startTime=$(date +%Y%m%d)
+#控制台日志存放目录
+consolePath=$path'logs/'
 #判断日志目录是否存在，不存在创建
 if [ ! -d $consolePath ];
 then
+    echo Creating a Log Directory:"$consolePath"
     mkdir -p $consolePath;
 fi
-
+consoleLog="$consolePath"console-"$startTime".log
+echo '日志路径:' "$consoleLog"
+#jar启动参数
+startupParameters='-Duser.timezone=GMT+8 -Dspring.profiles.active=prod'
 #若项目已启动，杀死旧进程
-api_pid=`ps -ef | grep "$javaPath/$app.jar" | grep -v grep | awk '{print $2}'`
-echo $api_pid
-
+api_pid=`ps -ef | grep "$java" | grep -v grep | awk '{print $2}'`
 if [ "$api_pid" != "" ]; then
-        echo kill api
+        echo The current JAR pid $api_pid
+        echo kill $api_pid
         kill -9 $api_pid
-
         echo sleep 3s
         sleep 1
         echo sleep 2s
         sleep 1
         echo sleep 1s
         sleep 1
+else
+        echo The current JAR is not running
 fi
 
-echo start jar
+echo starting "【nohup java -jar  $startupParameters $java >> $consoleLog 2>&1 &】"
 #后台进程形式启动项目
-nohup java -jar -Duser.timezone=GMT+8 -Dspring.profiles.active=$active $javaPath/$app.jar >> $consolePath/console-"$startTime".log 2>&1 &
+nohup java -jar  $startupParameters $java >> $consoleLog 2>&1 &
